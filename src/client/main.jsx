2567f82aaeec86cdada01b914a543d02eb35eb95
mustafapeksen <mustafapeksen04@gmail.com>
2024-07-16 13:41:51 +0300

    Add router, path and elemnt in website

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx';
+import Singer from './components/Singer.jsx';
+import NotFound from './components/NotFound.jsx';
+import HomePage from './components/HomePage.jsx';
+import Stories from './components/Stories.jsx';
+import Song from './components/Song.jsx';
+
+
+// Define the routes for the application using createBrowserRouter
+const router = createBrowserRouter([{
+  path: "/", // Root path
+  element: <App />, // Main application component
+  errorElement: <NotFound />, // Component to show when route is not found
+  children: [ // Nested routes
+    {
+      path: "", // Default route (home page)
+      element: <HomePage /> // HomePage component
+    },
+    {
+      path: "singer", // Path for 'singer' page
+      element: <Singer /> // Singer component
+    },
+    {
+      path: "song", // Path for 'song' page
+      element: <Song /> // Song component
+    },
+    {
+      path: "stories", // Path for 'stories' page
+      element: <Stories /> // Stories component
+    }]
+}])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
