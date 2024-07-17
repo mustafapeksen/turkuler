c7e545e1924a68e0d88472b0ca2b9fcba4517c0f
mustafapeksen <mustafapeksen04@gmail.com>
2024-07-17 13:41:45 +0300

    Prepare website home page

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -1,6 +1,60 @@
-import React from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function HomePage() {
-    return <h1>Home Page</h1>
+    const [randomSong, setRandomSong] = useState(null);
+    const [error, setError] = useState(null);
+
+    async function handleData() {
+        try {
+            const response = await axios.get('http://localhost:3000/random', {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            setRandomSong(response.data);
+        } catch (error) {
+            console.error('Error:', error); // Hata varsa konsolda görüntüle
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        handleData();
+    }, []);
+
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+
+    if (randomSong) {
+        return (
+            <section>
+                <figure>
+                    <iframe width="640" height="480" src={randomSong.url} title={randomSong.name} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </figure>
+                <article>
+                    <h2>{randomSong.name}</h2>
+                    <p style={{ whiteSpace: 'pre-line' }}>{randomSong.lyrics}</p>
+                    <a href={randomSong.lyricsSource.url}><small>{randomSong.lyricsSource.publication}</small></a>
+                </article>
+                <article>
+                    <h2>Hikayesi</h2>
+                    <p style={{ whiteSpace: 'pre-line' }}>{randomSong.story}</p>
+                    <a href={randomSong.storySource.url}><small>{randomSong.storySource.publication}</small></a>
+                </article>
+                <article>
+                    <figure>
+                        <img src="" alt={randomSong.singer} />
+                    </figure>
+                    <h3>{randomSong.singer}</h3>
+                </article>
+            </section>
+        );
+    } else {
+        return <h1>Loading ...</h1>
+    }
+
 }
+
 export default HomePage;
