a65ca9d5ae856f0e135a83ba7b1ce7132a327c6c
mustafapeksen <mustafapeksen04@gmail.com>
2024-07-30 13:29:39 +0300

    Update Stories.jsx file

--- src/client/components/Stories.jsx
+++ src/client/components/Stories.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Story from "./Story";
 function Stories() {
-    return <h1>Stories Page</h1>
+    const [songs, setSongs] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+
+        const fetchSongs = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/turkuler');
+                setSongs(response.data);
+            } catch (error) {
+                console.error('Error fetching songs:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchSongs();
+    }, []);
+
+    if (isLoading) {
+        return <h1>Loading ...</h1>
+    } else {
+        return (
+            <>
+                {songs.map((song) => (
+                    <Story
+                        key={song.id}
+                        name={song.name}
+                        singer={song.singer}
+                        story={song.story}
+                        publicationURL={song.storySource.url}
+                        publication={song.storySource.publication}
+                    />
+                ))}
+            </>
+        )
+    }
+
 }
 export default Stories;
